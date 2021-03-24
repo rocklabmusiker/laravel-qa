dc4b38d9a57fc0878582ec51bfdce1cadf4b9dda
Daniel_Wagner_mgo <daniel.wagner@glashuette-original.com>
2021-03-24 15:08:56 +0100

    Update best_answer_id on questions table when an answer removed usin Eloquent event

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -41,7 +41,14 @@ class Answer extends Model
         });
 
         static::deleted(function ($answer) {
-            $answer->question->decrement('answers_count');
+            $question = $answer->question;
+            $question->decrement('answers_count');
+
+            if ($question->best_answer_id == $answer->id) {
+                $question->best_answer_id = NULL;
+                $question->save();
+            }
+
         });
 
     }
@@ -49,5 +56,10 @@ class Answer extends Model
     public function getCreatedDateAttribute() 
     {
         return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute() 
+    {
+        return $this->id == $this->question->best_answer_id ? 'vote-accepted' : ''; 
     }
 }
